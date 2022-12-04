2da95c41ad61cd1c3dba3f27c1d5daaf3c6b0319
Artur-Lepczynski <artur.lepczynski@gmail.com>
2022-12-04 14:40:59 +0100

    add: cards basic functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,28 @@
 const wheel = document.querySelector("#wheel");
+let lastAngle; 
 
 wheel.addEventListener("click", (event)=>{
-    let angle = Math.floor(Math.random() * 5000);
+    let angle = getRandomNumber(3000);
+    if(Math.abs(lastAngle - angle) < 90) angle = 90; 
     wheel.style.transform = `rotate(${angle}deg)`;
-    console.log(angle);
+    lastAngle = angle; 
 });
 
 const decks = Array.from(document.querySelectorAll(".deck"));
-const flipped = [false, false, false];
 
 decks.forEach((item, i)=>{
     item.addEventListener("click", (event)=>{
-        
-        decks[i].src 
-        
+        if(decks[i].lastElementChild.classList.contains("card-hidden")){
+            let randomNumber = getRandomNumber(3);
+            decks[i].lastElementChild.src = `./images/Talia${i+1}/karta${randomNumber}.png`; 
+        }
+        decks[i].lastElementChild.classList.toggle("card-hidden");  
     });
 });
 
-
+function getRandomNumber(max){
+    return Math.floor(Math.random() * max) + 1; 
+};
 
 
 
